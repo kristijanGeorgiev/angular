00eabdbcc8ef097350d996b090c288eca2d72dcc
kristijanGeorgiev <148566162+kristijanGeorgiev@users.noreply.github.com>
2024-12-14 19:19:32 +0100

    Add files via upload

---
+++ episodes-id/src/app/episode-edit/episode-edit.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EpisodeEditComponent } from './episode-edit.component';
+
+describe('EpisodeEditComponent', () => {
+  let component: EpisodeEditComponent;
+  let fixture: ComponentFixture<EpisodeEditComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [EpisodeEditComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(EpisodeEditComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
