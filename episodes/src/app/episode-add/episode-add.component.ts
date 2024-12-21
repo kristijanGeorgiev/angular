b33d2737219ba7776eabf7babb9ff56e6ac4cd34
kristijanGeorgiev <148566162+kristijanGeorgiev@users.noreply.github.com>
2024-12-22 00:56:34 +0100

    Add files via upload

---
+++ episodes/src/app/episode-add/episode-add.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Episode} from '../models/episode';
+import { EpisodeService } from '../services/episode.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-episode-add',
+  standalone: false,
+  
+  templateUrl: './episode-add.component.html',
+  styleUrl: './episode-add.component.css'
+})
+export class EpisodeAddComponent implements OnInit{
+  newEpisode: Episode= {id: 0, title: '', series: 0, era: '', broadcast_date: '', director: '', writer: '', plot: '', doctor: {actor: '', incarnation: ''}, companion: {actor: '', character: ''}, cast: []}
+  director: string[] = [];
+  constructor(private episodeService: EpisodeService, private router: Router) {}
+ ngOnInit(): void {
+   this.getDirectors();
+ }
+ addEpisode(): void {
+   this.episodeService.addEpisode(this.newEpisode).subscribe(() => {
+     this.router.navigate(['/episodes']);
+   });
+ }
+ goToEpisodeList(): void {
+   this.router.navigate(['/episodes']);
+ }
+getDirectors(): void {
+ this.episodeService.getDirectors().subscribe((director) => {
+   this.director = director;
+ });
+}
+}
---
+++ episodes/src/app/models/episode.ts
@@ -0,0 +1,29 @@
+export interface Episode {
+    id: number;
+    title: string;
+    series: number;
+    era: string;
+    broadcast_date: string;
+    director: string;
+    writer: string;
+    plot: string;
+    doctor: Doctor;
+    companion: Companion;
+    cast: Cast[];
+}
+export interface Doctor {
+    actor: string;
+    incarnation: string;
+}
+export interface Companion {
+    actor: string;
+    character: string;
+}
+export interface Cast {
+    actor: string;
+    character: string;
+}
+export interface Actor {
+    id: number;
+    name: string;
+}
